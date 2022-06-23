de894ba806c1a0e92a0c2a2c5419fc5465ca8db7
Joe <joelee@utechzone.com.tw>
2022-06-23 12:51:12 +0800

    新增  HomeController KeyWordSearch 商品關鍵字搜尋

--- ShoppingCar/ShoppingCar/Controllers/HomeController.cs
+++ ShoppingCar/ShoppingCar/Controllers/HomeController.cs
@@ -17,11 +17,18 @@ namespace ShoppingCar.Controllers
 
         public ActionResult Index()
         {
-            //查詢商品
+            //查詢全部商品
             var Products = db.Products.OrderByDescending(m => m.Id).ToList();
             return View(Products);
         }
 
-        
+        public ActionResult KeyWordSearch(string key)
+        {
+            //商品關鍵字搜尋
+            var products = db.Products.Where(m => m.Name.Contains(key))
+                .OrderByDescending(m => m.Id).ToList();
+            return View("Index", products);
+        }
+
     }
 }
