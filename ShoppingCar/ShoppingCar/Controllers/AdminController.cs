603a35801e2c49093823d9031b3bdb1d9750a604
bj026080 <bj026080@gmail.com>
2022-06-26 16:21:45 +0800

    新增  AdminController AddProduct 新增商品

--- ShoppingCar/ShoppingCar/Controllers/AdminController.cs
+++ ShoppingCar/ShoppingCar/Controllers/AdminController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ShoppingCar.Models;
+using System.IO;
 
 namespace ShoppingCar.Controllers
 {
@@ -21,27 +22,60 @@ namespace ShoppingCar.Controllers
             return View(product);
         }
 
-        // GET: Products/Create
-        public ActionResult Create()
+        public ActionResult AddProduct()
         {
             return View();
         }
 
-        // POST: Products/Create
-        // 若要免於大量指派 (overposting) 攻擊，請啟用您要繫結的特定屬性，
-        // 如需詳細資料，請參閱 https://go.microsoft.com/fwlink/?LinkId=317598。
+        bool IsExisting(string target_ID)
+        {
+            //檢查商品ID是否已存在
+            var Product = db.Products.Where(m => m.PId == target_ID).FirstOrDefault();
+            return Product != null;
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,PId,Name,Price,Img")] Product product)
+        public ActionResult AddProduct([Bind(Include = "Id,PId,Name,Price,Img")] Product product, HttpPostedFileBase ProductImg)
         {
-            if (ModelState.IsValid)
+            //新增商品
+            if (!ModelState.IsValid)
+                return View(product);
+
+            //檢查商品ID是否已存在
+            if (IsExisting(product.PId))
             {
-                db.Products.Add(product);
+                ViewBag.Error = "此商品ID已存在";
+                return View();
+            }
+
+            //新增商品
+            try
+            {
+                string FileName = "";
+                //存圖
+                if (ProductImg != null && ProductImg.ContentLength > 0)
+                {
+                    FileName = Guid.NewGuid().ToString() + ".jpg";
+                    string path = Path.Combine(Server.MapPath("~/images"), FileName);
+                    ProductImg.SaveAs(path);
+                }
+
+                //新增產品資訊
+                Product NewProduct = new Product();
+                NewProduct.PId = product.PId;
+                NewProduct.Img = FileName;
+                NewProduct.Name = product.Name;
+                NewProduct.Price = product.Price;
+                db.Products.Add(NewProduct);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            return View(product);
+            catch (Exception e)
+            {
+                ViewBag.Error = e.ToString();
+                return View();
+            }
         }
 
         // GET: Products/Edit/5
